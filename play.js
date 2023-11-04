97036f024ad2ba2e760ff59cd99c22e5519706d4
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-11-04 17:42:30 +0530

    added seperate file for handlers

--- play.js
+++ play.js
@@ -1,39 +1,8 @@
 const http = require('http')
-const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes')
 
-    if(url === '/'){
-        fs.readFile('message.txt',{encoding :'utf-8'},(err,data) => {
-            if(err){
-                console.log(err);
-            }
-            res.write('<html>')
-            res.write('<head><title>First page</title></head>')
-            res.write('<body>')
-            res.write(`<h3>${data}</h3>`)
-            res.write('<form action="/message" method="POST"><input type="text" name="message"><button>Save</button></form>')
-            res.write('</body>')
-            res.write('</html>')
-            return res.end();
-        })
-    }
-    if(url === '/message' && method === "POST"){
-        const body = [];
-        req.on('data',(chunk) => {
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-            fs.writeFileSync('message.txt',message);
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-})
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
