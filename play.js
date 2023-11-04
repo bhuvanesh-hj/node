86cca9d853b06fb83038f20ebe122d6c5cbe616e
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-11-04 11:44:13 +0530

    created routes

--- play.js
+++ play.js
@@ -1,7 +1,32 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    console.log('Bhuvanesh HJ')
+    const url = req.url;
+
+    if(url === '/home'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>Welcome Home</h1>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if(url === '/about'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>Welcome About us page</h1>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if(url === '/node'){
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<h1>Welcome node js project</h1>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
 })
 
 server.listen(4000);
