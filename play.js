386c27a6569c3eea0e3b51bcc95e3bac5a09a813
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-11-06 19:05:45 +0530

    added the size input

--- play.js
+++ play.js
@@ -1,16 +1,21 @@
 // const http = require("http");
 
-const expres = require("express");
+const express = require("express");
+const body = require("body-parser");
 
-const app = expres();
+const app = express();
 
-app.use((req, res, next) => {
-  console.log("Hello using the first middleware");
-  next();
+app.use(body.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button>Add product</button></form>')
 });
 
-app.use((req, res, next) => {
-  console.log("Hello using the Second middleware");
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect("/")
+})
+app.use('/',(req, res, next) => {
   res.send('<h1>Hello World!</h1>')
 });
 
