7936eafaad705e083abff6a03fc54bec3ac438d7
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-11-06 15:32:12 +0530

    Using the express js

--- play.js
+++ play.js
@@ -1,9 +1,23 @@
-const http = require('http')
+// const http = require("http");
 
-const routes = require('./routes')
+const expres = require("express");
 
+const app = expres();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+  console.log("Hello using the first middleware");
+  next();
+});
 
-server.listen(4000);
+app.use((req, res, next) => {
+  console.log("Hello using the Second middleware");
+  res.send('<h1>Hello World!</h1>')
+});
 
+app.listen(3000);
+
+// const routes = require("./routes");
+
+// const server = http.createServer(routes.handler);
+
+// server.listen(4000);
